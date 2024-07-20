80c5f42136815bdc8f5dbde5973a708ab3166936
SaidValenzuela <alejandrovalenzuela2543@gmail.com>
2024-07-20 14:08:59 -0600

    Iniciar sesion y registrarse

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -10,7 +10,13 @@ class InicioController extends Controller
     public function inicioTurismoLosAngeles(){
         return view('inicio-turismo-los-angeles');
     }
-    // public function logearTurismoLosAngeles(){
-    //     return view('inicioSesionTurismoLosAngeles');
-    // }
+    
+    public function logearTurismoLosAngeles(){
+        return view('iniciarSesionTurismoLosAngeles');
+    }
+
+    public function registrarseTurismoLosAngeles(){
+        return view('registrarseTurismoLosAngeles');
+    }
+    
 }
---
+++ resources/views/registrarseTurismoLosAngeles.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/registrarse.css') }}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="wrapper">
+       <div class="form-box">
+             <div class="register-container" id="register">
+          <div class="top">
+            <span>¿Ya tienes cuenta? <a href="/iniciar-sesion" onclick="login()">Iniciar sesión</a></span>
+            <header>Registrate</header>
+          </div>
+          <div class="two-forms">
+            <div class="input-box">
+              <input type="text" class="input-field" placeholder="Nombre">
+              <i class="bx bx-user"></i>
+            </div>
+            <div class="input-box">
+              <input type="text" class="input-field" placeholder="Apellido">
+              <i class="bx bx-user"></i>
+            </div>
+          </div>
+          <div class="input-box">
+            <input type="email" class="input-field" placeholder="Email">
+            <i class="bx bx-envelope"></i>
+          </div>
+          <div class="input-box">
+            <input type="password" class="input-field" placeholder="Contraseña">
+            <i class="bx bx-lock-alt"></i>
+          </div>
+          <div class="input-box">
+            <input type="submit" class="submit" value="Registrate">
+          </div>
+         </div>
+       </div>
+      </div>
+    
+</body>
+</html>
