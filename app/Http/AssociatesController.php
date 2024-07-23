41ef663cd6fa472e7000fcea50bb6ee7b7a98ba1
Alessandro <sifuentesmarcelo78@gmail.com>
2024-07-22 18:33:12 -0600

    [ADD]Cruds Clientes

---
+++ app/Http/AssociatesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Associate;
+
+class AssociatesController extends Controller{
+    public function view(){
+        return view('adminFold.clientes');
+    }
+
+    public function index()
+    {
+        return response()->json(Associate::all(), 200);
+    }
+
+    public function show($id)
+    {
+        return response()->json(Associate::find($id), 200);
+    }
+
+    public function store(Request $request)
+    {
+        $associate = Associate::create($request->all());
+        return response()->json($associate, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $associate = Associate::findOrFail($id);
+        $associate->update($request->all());
+        return response()->json($associate, 200);
+    }
+
+    public function destroy($id)
+    {
+        Associate::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AssociatesController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 
@@ -15,17 +16,27 @@ Route::get('/logear-turismo-los-angeles', [UserController::class, 'vistaLogin'])
 Route::post('/logear-turismo-los-angeles', [UserController::class, 'logear']);
 Route::post('/registrar-turismo-los-angeles', [UserController::class, 'registrar']);
 
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect('/inicio-turismo-los-angeles');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
+//     return redirect('/inicio-turismo-los-angeles');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
 
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-    return back()->with('status', 'verification-link-sent');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
+//     return back()->with('status', 'verification-link-sent');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
+Route::get('/dashboard', function(){
+    return view('adminFold.dashboard');
+});
+
+Route::get('/view/associates',[AssociatesController::class, "view"])->name('associates');
+Route::get('/get/associates', [AssociatesController::class, "index"]);
+Route::get('/get/associate/{id}',[AssociatesController::class, "show"]);
+Route::post('/insert/associate',[AssociatesController::class, "store"]);
+Route::put('/update/associate/{id}',[AssociatesController::class, "update"]);
+Route::delete('/delete/associate/{id}',[AssociatesController::class, "destroy"]);
